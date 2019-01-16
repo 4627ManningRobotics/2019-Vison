4b96c892241d9fe5ddb386877d76c1488bc45ed3
lucas-brown <lucasbrown222@gmail.com>
2019-01-16 04:02:04 +0000

    read more bytes and eliminated extra thread

--- RasPi/OpenMV_IN.py
+++ RasPi/OpenMV_IN.py
@@ -7,30 +7,27 @@ import queue
 class Reciever:
 
 	def __init__(self):
-		self.openMVIn = serial.Serial('/dev/ttyUSB0', baudrate = 115200, timeout = 0)
-		self.message = ""
-		self.decode_queue = queue.Queue(10000) #yes this is a MASSIVE queue
+		self._openMVIn = serial.Serial('/dev/ttyUSB0', baudrate = 115200, timeout = 0)
+		self._buffer = u''
+		#self.decode_queue = queue.Queue(10000) #yes this is a MASSIVE queue
 		self.messages = queue.Queue(100)
 
-		self.decoder_thread = threading.Thread( target = self.decode_loop )
-		self.decoder_thread.daemon = True
-		self.decoder_thread.start()
+		#self.decoder_thread = threading.Thread( target = self.decode_loop )
+		#self.decoder_thread.daemon = True
+		#self.decoder_thread.start()
 
 	def in_loop(self):
 		while True:
-			input = self.openMVIn.read(1)
-			self.decode_queue.put_nowait(input)
-
-	def decode_loop(self):
-		while True:
-			c = self.decode_queue.get(2)
-			if c == b'\n':
+			input = self._openMVIn.read(1024)
+			self._buffer += input.decode("utf-8", "ignore")
+			
+			while u'\n' in self._buffer:
+				idx = self._buffer.find(u'\n')
+				message = self._buffer[:idx]
+				self._buffer = self._buffer[idx + 1:]
 				if self.messages.full():
-					self.messages.get(2) #waste least recent
-				self.messages.put(self.message, 2)
-				self.message = ""
-			else:
-				self.message += c.decode("utf-8", "ignore")
+					self.messages.get(2)
+				self.messages.put(message) 
 
 	def get_message(self):
 		if(self.messages.empty()):
